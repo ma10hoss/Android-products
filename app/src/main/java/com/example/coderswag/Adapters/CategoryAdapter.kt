ee6ddc24eb3eb56e0cc1be9bfb27fce7fac8c8bd
Arman Hossain <ma10hoss@siena.edu>
2018-10-09 09:28:32 -0400

    lesson 4

--- app/src/main/java/com/example/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryAdapter.kt
@@ -25,20 +25,41 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView : View
+        val holder : ViewHolder
+
+        if (convertView == null){
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item,null)
+            holder = ViewHolder()
+            //holder instead of val is more efficient
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+            //println("I exist for the first time")
+//            val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
+//            val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
+
+                // setting a unique value to the categoryView, which is the holder.
+            categoryView.tag = holder
+        }else{
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+            //println("Go green recycle")
+        }
                                             // we can null this however we need to remember to select out file after "R"
 
         // no idea what im doing
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item,null)
-        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
-        println("Heavy Computing")
+
+        //not needed
+        //println("Heavy Computing")
 
         val category = categories[position]
 
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryImage.setImageResource(resourceId)
-        println(resourceId)
-        categoryName.text = category.title
+        /**after adding the else section we need to add holder infront of caterogyImage---
+         ? are added to make it a safe call because they are nullable */
+        holder.categoryImage?.setImageResource(resourceId)
+            //print line not needed
+        //println(resourceId)
+        holder.categoryName?.text = category.title
         return categoryView
     }
 
@@ -53,5 +74,9 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
     override fun getCount(): Int {
         return categories.count()
     }
-
+    // allows us to reuse and recycle our views
+    private class ViewHolder{
+        var categoryImage:ImageView?= null
+        var categoryName: TextView?= null
+    }
 }
