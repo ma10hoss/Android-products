0fdc8d8d9c1c341017575abd5d8ac1306aac9d87
Arman Hossain <ma10hoss@siena.edu>
2018-10-12 12:32:07 -0400

    lesson 7, changed Date Services and added a product_list_item layout.xml

--- app/src/main/java/com/example/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -21,23 +21,45 @@ object DataService {
     )
 
     val hats = listOf(
-            Product("Devslopes Grafic Beanie","$18","hat01"),
-            Product("Devslopes Hat Black","$20","hat02"),
-            Product("Devslopes Hat White","$22","hat03"),
-            Product("Devslopes Hat Snapback","$24","hat04")
+            Product("Devslopes Grafic Beanie","$18","hat1"),
+            Product("Devslopes Hat Black","$20","hat2"),
+            Product("Devslopes Hat White","$22","hat0"),
+            Product("Devslopes Hat Snapback","$24","hat4")
     )
     val hoodies = listOf(
-            Product("Devslopes Hoodie Gray","$28","hoodie01"),
-            Product("Devslopes Hoodie Red","$32","hoodie02"),
-            Product("Devslopes Hoodie Gray","$28","hoodie03"),
-            Product("Devslopes Hoodie Black","$32","hoodie04")
+            Product("Devslopes Hoodie Gray","$28","hoodie1"),
+            Product("Devslopes Hoodie Red","$32","hoodie2"),
+            Product("Devslopes Hoodie Gray","$28","hoodie3"),
+            Product("Devslopes Hoodie Black","$32","hoodie4")
     )
-    val shirts = listOf( Product("Devslopes shirt Gray","$17","shirt01"),
-            Product("Devslopes badge light gray","$18","shirt02"),
-            Product("Devslopes logo shirt red","$19","shirt03"),
-            Product("Devslopes Hoodie hustle","$20","shirt04"),
-            Product("KickFlip Studio","$18","shirts05")
+    val shirts = listOf( Product("Devslopes shirt Gray","$17","shirt1"),
+            Product("Devslopes badge light gray","$18","shirt2"),
+            Product("Devslopes logo shirt red","$19","shirt3"),
+            Product("Devslopes Hoodie hustle","$20","shirt4"),
+            Product("KickFlip Studio","$18","shirts5")
     )
+        // this is an empty array for digital goods.
+    val digitalGood = listOf<Product>()
+
+    fun getProducts(category: String) : List<Product>{
+
+            // alt enter wasnt working to lift return out of each categories.
+        return when(category) {
+            "SHIRTS" ->  shirts
+            "HATS" ->  hats
+            "Hoodies" ->  hoodies
+            // exaustive so you need an else statement
+            else -> digitalGood
+
+        }
+        // this is similar to---- but more coding.
+//        if (category == "HATS"){
+//            return hats
+//        } else if (category == "SHIRTS"){
+//            return shirts
+
+//        etc........
+    }
 
 
 
